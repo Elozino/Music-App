67267b7361b1ca899bc73c852f6a71172a0ee1a2
Elozino <ovedhee@gmail.com>
2021-11-13 12:15:32 +0100

    added functionalities

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const playlists = document.querySelector(".playlists");
 
+//Song Info
 const trackImage = document.querySelector(".track-image");
 const songTitle = document.querySelector(".song-title");
 const artisteName = document.querySelector(".artiste-name");
+
+//Speaker Volume
+const speakerVolume = document.querySelector(".speaker-volume");
+const speakerControl = document.querySelector(".speaker-control");
+const speaker = document.querySelector(".speaker");
 
 //Controls
 const prev = document.querySelector(".previous-btn");
@@ -14,12 +20,24 @@ const play = document.querySelector(".play-pause-btn");
 const next = document.querySelector(".next-btn");
 const playall = document.querySelector(".playall");
 
+//Song duration
+const currentTimer = document.querySelector(".current-time");
+const durationTime = document.querySelector(".duration-time");
+const durationSlider = document.querySelector(".duration-slider");
+console.log(durationTime);
+console.log(currentTimer);
+console.log(durationSlider)
+
+
+//To open and close playlist section
 menu.addEventListener("click", () => {
   playlists.style.left = "0";
 });
 close.addEventListener("click", () => {
   playlists.style.left = "-100%";
 });
+
+
 
 //Creating a track list
 let trackList = [
@@ -49,9 +67,17 @@ let trackList = [
   },
 ];
 
+
+//EventListeners
 play.addEventListener("click", playAndPause);
+next.addEventListener("click", nextSong);
+prev.addEventListener("click", prevSong);
+speaker.addEventListener("click", mute);
+speakerControl.addEventListener("change", slider);
+durationSlider.addEventListener("change", updateDuration);
 
 let indexOfTrack = 0;
+let timer = 0;
 let track = document.createElement("audio");
 let isPlaying = false;
 
@@ -61,26 +87,76 @@ function loadTracks(indexTrack) {
   trackImage.src = trackList[indexTrack].img;
   songTitle.textContent = trackList[indexTrack].name;
   artisteName.TextContent = trackList[indexTrack].artiste;
-  //track.load();
+  track.load();
 }
 loadTracks(indexOfTrack);
 
-
-function playAndPause(){
-    if(isPlaying == false){
-        playSong()
-    }else{
-        pauseSong();
-    }
+//Play and Paused tracks
+function playAndPause() {
+  if (isPlaying == false) {
+    playSong();
+  } else {
+    pauseSong();
+  }
 }
+
+//Play tracks
 function playSong() {
   track.play();
   isPlaying = true;
   play.innerHTML = '<i class="fas fa-pause"></i>';
 }
 
+//Pause tracks
 function pauseSong() {
-    track.pause()
-    isPlaying = false;
-    play.innerHTML = '<i class="fas fa-play"></i>';
+  track.pause();
+  isPlaying = false;
+  play.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+//Next Song
+function nextSong() {
+  if (indexOfTrack < trackList.length - 1) {
+    indexOfTrack++;
+    loadTracks(indexOfTrack);
+    playSong();
+  } else {
+    indexOfTrack = 0;
+    loadTracks(indexOfTrack);
+    playSong();
+  }
+}
+
+//Previous song
+function prevSong() {
+  if (indexOfTrack > 0) {
+    indexOfTrack--;
+    loadTracks(indexOfTrack);
+    playSong();
+  } else {
+    indexOfTrack = trackList.length - 1;
+    loadTracks(indexOfTrack);
+    playSong();
+  }
+}
+
+//To mute sound
+function mute() {
+  track.volume = 0;
+  speakerVolume.innerHTML = 0;
+  speakerControl.value = 0;
+  console.log(speaker);
+}
+
+//To change volume with th slider
+function slider() {
+  speakerVolume.value = speakerControl.value;
+  track.volume = speakerControl.value / 100;
+  speakerVolume.innerHTML = Math.round(track.volume * 100);
+}
+
+//To update duration of audio
+function updateDuration(){
+  console.log('changed')
+  console.log(track.currentTime)
 }
